426f521695b0d27922ab08a001149c0764379a74
lsj8367 <lsj8367@naver.com>
2023-12-17 21:33:40 +0900

    feat: add p13

---
+++ src/test/java/승재/ch8/P13Test.java
@@ -0,0 +1,36 @@
+package 승재.ch8;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class P13Test {
+
+    @ParameterizedTest
+    @MethodSource
+    void test(ListNode node) {
+        //given
+        final P13 p13 = new P13();
+
+        //when
+        final boolean expected = p13.isPalindrome(node);
+
+        //then
+        assertThat(expected).isTrue();
+    }
+
+    private static Stream<Arguments> test() {
+        return Stream.of(
+                Arguments.of(new ListNode(1,
+                        new ListNode(2, new ListNode(2, new ListNode(1))))),
+                Arguments.of(new ListNode(1,
+                        new ListNode(2, new ListNode(3, new ListNode(2, new ListNode(1))))))
+        );
+    }
+
+}

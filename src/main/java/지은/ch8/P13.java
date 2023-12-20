e3a4b9ab92b646d01938b44856ffdd080811eded
jexnjeux <zieun.na@gmail.com>
2023-12-21 00:11:05 +0900

    Add [ch8/P13] L234.팰린드롬 연결 리스트

---
+++ src/main/java/지은/ch8/P13.java
@@ -0,0 +1,33 @@
+package 지은.ch8;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class P13 {
+
+    public boolean isPalindrome(ListNode head) {
+        Deque<Integer> deque = new LinkedList<>();
+
+        ListNode node = head;
+        while (node != null) {
+            deque.add(node.val);
+            node = node.next;
+        }
+        while (deque.size() > 1) {
+            if (!deque.pollFirst().equals(deque.pollLast())) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        ListNode node5 = new ListNode(1);
+        ListNode node4 = new ListNode(2, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+
+        P13 p13 = new P13();
+        System.out.println(p13.isPalindrome(node1));
+    }
+}

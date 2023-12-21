cb63ddc39d7093474f1e0561e7318e49dc326dfe
jexnjeux <zieun.na@gmail.com>
2023-12-22 04:31:41 +0900

    Add : [ch8/P14] L21.두 정렬 리스트의 병합

---
+++ src/main/java/지은/ch8/P14.java
@@ -0,0 +1,19 @@
+package 지은.ch8;
+
+public class P14 {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        } else if (list1 == null) {
+            return list2;
+        }
+        return list1;
+    }
+}

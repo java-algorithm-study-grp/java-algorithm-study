c73002641d92798fd283d73ec3398d9692bcc4fd
wxxhyeong <wxxhyeong@gmail.com>
2023-12-25 20:34:37 +0900

    add : [ch8/P13] L234. 팰린드롬 연결 리스트

---
+++ src/main/java/우형/ch6/P6.java
@@ -0,0 +1,11 @@
+package src.main.java.우형.ch6;
+
+public class P6 {
+    public String longestPalindrome(String s) {
+        /*
+        팰린드롬 문자열을 찾기 때문에 대칭되는 문자열인지 확인해야한다.
+        대칭되는 문자열이 없을 경우 가장 긴 팰린드롬 문자열은
+        매개변수 s의 0번째 index가 return 돼야한다.
+         */
+    }
+}

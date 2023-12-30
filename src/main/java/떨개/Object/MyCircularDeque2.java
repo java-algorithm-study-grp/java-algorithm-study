7dc76e004bdbfe9b2764b1c3339dc40e890af210
Mun Kyoung-min <mun9659@naver.com>
2023-12-30 21:45:29 +0900

    Add : [ch10/P26] L641. 원형 테크 디자인

---
+++ src/main/java/떨개/Object/MyCircularDeque2.java
@@ -0,0 +1,58 @@
+package 떨개.Object;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MyCircularDeque2 {
+
+    // Deque를 사용하는 방법
+
+    int k;
+    Deque<Integer> deque = new LinkedList<>();
+
+    public MyCircularDeque2(int k) {
+        // 데크 크기를 k로 지정하는 생성자
+        this.k = k;
+    }
+
+    public boolean insertFront(int value) {
+        if(isFull()) return false;
+        return deque.offerFirst(value);
+    }
+
+    public boolean insertLast(int value) {
+        if(isFull()) return false;
+        return deque.offerLast(value);
+    }
+
+    public boolean deleteFront() {
+        if(isEmpty()) return false;
+        return deque.pollFirst() != null;
+    }
+
+    public boolean deleteLast() {
+        if(isEmpty()) return false;
+        return deque.pollLast() != null;
+    }
+
+    public int getFront() {
+        return deque.getFirst();
+    }
+
+    public int getRear() {
+        return deque.getLast();
+    }
+
+    public boolean isEmpty() {
+        return deque.isEmpty();
+    }
+
+    public boolean isFull() {
+        return deque.size() == k;
+    }
+
+    @Override
+    public String toString() {
+        return deque.toString();
+    }
+}

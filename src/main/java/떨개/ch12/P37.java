20544098e52a83fac9b3f904de829d22649d254e
Mun Kyoung-min <mun9659@naver.com>
2024-01-07 14:50:55 +0900

    Fix : [ch12/P37] L46. 순열

--- src/main/java/떨개/ch12/P37.java
+++ src/main/java/떨개/ch12/P37.java
@@ -19,7 +19,8 @@ public class P37 {
         //                [4,1,2],
         //                [4,2,1]
         //               ]
-        int[] input = new int[] {1,2,4};
+        // int[] input = new int[] {1,2,4};
+        int[] input = new int[] {1,2,4,5,7};
 
         System.out.println(answer1(input));
 
@@ -29,13 +30,28 @@ public class P37 {
 
         // 1. DFS를 사용
         //  - 스스로 풀지 못하여 책과 인터넷을 확인. 다시 지우고 풀어볼 예정
+        //  - 인터넷에 있는 순열을 기반으로 재 생성, 순열에서 계속 visited를 사용하려는게 문제였음.
         List<List<Integer>> results = new ArrayList<>();
+
+        dfs1(results, input, new ArrayList<>(), input.length);
 
         return results;
     }
 
-    private static void dfs(List<List<Integer>> results, int[] input, boolean[] visited, List<Integer> list, int index) {
+    private static void dfs1(List<List<Integer>> results, int[] input, List<Integer> list, int count) {
 
+        if(count == 0) {
+            System.out.println("results = " + results + " list = " + list);
 
+            results.add(list.stream().toList());
+            return;
+        }
+
+        for (int i : input) {
+            if (list.contains(i)) continue;
+            list.add(i);
+            dfs1(results, input, list, count - 1);
+            list.remove(Integer.valueOf(i));
+        }
     }
 }

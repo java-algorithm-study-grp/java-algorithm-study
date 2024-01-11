d95fe94d0dc7156ff8c05a0edb0cc28872156f98
jooany <jjj423888@gmail.com>
2024-01-12 03:08:39 +0900

    Add : [ch14/p51] L617.두 이진 트리 병합

---
+++ src/main/java/주앤/ch14/TreeNode.java
@@ -0,0 +1,14 @@
+package 주앤.ch14;
+
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode() {}
+	TreeNode(int val) { this.val = val; }
+	TreeNode(int val, TreeNode left, TreeNode right) {
+	this.val = val;
+	this.left = left;
+	this.right = right;
+	}
+}

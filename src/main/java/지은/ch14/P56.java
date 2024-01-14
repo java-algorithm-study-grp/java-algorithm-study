ab6e130879bb5de42818e22efb48bbd129085ba6
jexnjeux <zieun.na@gmail.com>
2024-01-14 23:13:35 +0900

    Add : [ch14/P56] L1038.이진 탐색 트리를 더 큰 수 합계 트리로

---
+++ src/main/java/지은/ch14/P56.java
@@ -0,0 +1,28 @@
+package 지은.ch14;
+/*
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+Example 1:
+Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+
+ */
+public class P56 {
+    int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        bstToGst(root.right);
+        sum += root.val;
+        root.val = sum;
+        bstToGst(root.left);
+        return root;
+    }
+}
